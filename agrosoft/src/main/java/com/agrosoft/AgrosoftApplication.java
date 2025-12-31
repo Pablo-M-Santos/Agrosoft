132b2e17a75e854eba118830ab57934b22565417
Pablo Moreira Santos <pablomoreirasantos.hp@gmail.com>
2025-12-30 21:32:20 -0300

    feat: Creation and connection of the AgroSoft project #01

---
+++ agrosoft/src/main/java/com/agrosoft/AgrosoftApplication.java
@@ -0,0 +1,13 @@
+package com.agrosoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgrosoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgrosoftApplication.class, args);
+	}
+
+}
---
+++ agrosoft/src/test/java/com/agrosoft/AgrosoftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agrosoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgrosoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
