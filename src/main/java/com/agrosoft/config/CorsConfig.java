7aa177c7e02581f124e7f197ecac27ff65f63c33
Pablo Moreira Santos <pablomoreirasantos.hp@gmail.com>
2026-01-21 15:18:01 -0300

    feat: alteraçãoes no cors

--- src/main/java/com/agrosoft/config/CorsConfig.java
+++ src/main/java/com/agrosoft/config/CorsConfig.java
@@ -11,7 +11,8 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(
-                        "http://localhost:4200"
+                        "http://localhost:4200",
+                        "https://lucky-dodol-09dbff.netlify.app"
                 )
                 .allowedMethods(
                         "GET",
